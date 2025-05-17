6accbaf268c607d9b938e38f929fce18ebb8b705
amauri-castro <amauri.p.castro@gmail.com>
2025-05-16 22:27:16 -0300

    Iniciado implementação do microserviço de device-management

---
+++ src/main/java/com/amauri/algasensors/device/management/api/controller/SensorController.java
@@ -0,0 +1,44 @@
+package com.amauri.algasensors.device.management.api.controller;
+
+import com.amauri.algasensors.device.management.api.model.SensorInput;
+import com.amauri.algasensors.device.management.api.model.SensorOutput;
+import com.amauri.algasensors.device.management.common.IdGenerator;
+import com.amauri.algasensors.device.management.domain.model.Sensor;
+import com.amauri.algasensors.device.management.domain.model.SensorId;
+import com.amauri.algasensors.device.management.domain.repository.SensorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/sensors")
+public class SensorController {
+
+    private final SensorRepository sensorRepository;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public SensorOutput create(@RequestBody SensorInput input) {
+        Sensor sensor = Sensor.builder()
+                .id(new SensorId(IdGenerator.generateTSID()))
+                .name(input.getName())
+                .ip(input.getIp())
+                .location(input.getLocation())
+                .protocol(input.getProtocol())
+                .model(input.getModel())
+                .enabled(false)
+                .build();
+        sensor = sensorRepository.saveAndFlush(sensor);
+        return SensorOutput.builder()
+                .id(sensor.getId().getValue())
+                .name(sensor.getName())
+                .ip(sensor.getIp())
+                .location(sensor.getLocation())
+                .protocol(sensor.getProtocol())
+                .model(sensor.getModel())
+                .enabled(sensor.getEnabled())
+                .build();
+    }
+
+}
