a18a7c0522eb21db9c807892b2577de4137ff957
amauri-castro <amauri.p.castro@gmail.com>
2025-05-28 07:45:52 -0300

    Alterado rest client para usar padrao declarativo do spring implementando a interface com anotações

--- src/main/java/com/amauri/algasensors/device/management/api/client/impl/SensorMonitoringClientImpl.java
+++ src/main/java/com/amauri/algasensors/device/management/api/client/impl/SensorMonitoringClientImpl.java
@@ -7,8 +7,9 @@ import io.hypersistence.tsid.TSID;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
-@Component
-public class SensorMonitoringClientImpl implements SensorMonitoringClient {
+//não está sendo mais usando
+//@Component
+public class SensorMonitoringClientImpl /* implements SensorMonitoringClient */ {
 
     private final RestClient restClient;
 
@@ -16,27 +17,27 @@ public class SensorMonitoringClientImpl implements SensorMonitoringClient {
         this.restClient = factory.temperatureMonitoringRestClient();
     }
 
-    @Override
-    public void enableMonitoring(TSID sensorId) {
-        restClient.put()
-                .uri("/api/sensors/{sensorId}/monitoring/enable", sensorId)
-                .retrieve()
-                .toBodilessEntity();
-    }
-
-    @Override
-    public void disableMonitoring(TSID sensorId) {
-        restClient.delete()
-                .uri("/api/sensors/{sensorId}/monitoring/enable", sensorId)
-                .retrieve()
-                .toBodilessEntity();
-    }
-
-    @Override
-    public SensorMonitoringOutput getDetail(TSID sensorId) {
-        return restClient.get()
-                .uri("/api/sensors/{sensorId}/monitoring", sensorId)
-                .retrieve()
-                .body(SensorMonitoringOutput.class);
-    }
+//    @Override
+//    public void enableMonitoring(TSID sensorId) {
+//        restClient.put()
+//                .uri("/api/sensors/{sensorId}/monitoring/enable", sensorId)
+//                .retrieve()
+//                .toBodilessEntity();
+//    }
+//
+//    @Override
+//    public void disableMonitoring(TSID sensorId) {
+//        restClient.delete()
+//                .uri("/api/sensors/{sensorId}/monitoring/enable", sensorId)
+//                .retrieve()
+//                .toBodilessEntity();
+//    }
+//
+//    @Override
+//    public SensorMonitoringOutput getDetail(TSID sensorId) {
+//        return restClient.get()
+//                .uri("/api/sensors/{sensorId}/monitoring", sensorId)
+//                .retrieve()
+//                .body(SensorMonitoringOutput.class);
+//    }
 }
